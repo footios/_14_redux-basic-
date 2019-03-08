2991e6186c973be011bcda5a8053305d74883fe8
Fotis Tsakiris <footios76@gmail.com>
2019-03-08 07:55:59 +0200

    intro to redux - redux.basics.js

--- redux-basics.js
+++ redux-basics.js
@@ -5,3 +5,44 @@ instead, just to show the different concepts of redux
 in one file and to show that it's independent
 of react. */
 
+// NodeJS syntax:
+const redux = require('redux')
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.val
+        }
+    }
+    return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+
+
+
+// Subscription (comes before the actions)
+store.subscribe(() => {
+    console.log('[Subscription', store.getState())
+})
+
+// Dispaching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', val: 10});
+
+console.log(store.getState());
+
