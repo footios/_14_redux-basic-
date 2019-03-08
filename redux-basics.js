0a1a91771b01b9fb905fa9de12913a2b56e7e1f8
Fotis Tsakiris <footios76@gmail.com>
2019-03-08 07:16:39 +0200

    initial

---
+++ redux-basics.js
@@ -0,0 +1,7 @@
+/* Now this file here will not be holding any react code, 
+we won't include it into our react project, I'll
+execute it with nodeJS
+instead, just to show the different concepts of redux 
+in one file and to show that it's independent
+of react. */
+
